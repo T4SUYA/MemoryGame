38250db669720f3dc5ca07b06963257b7a001713
João Victor <54009514+T4SUYA@users.noreply.github.com>
2021-07-22 20:08:55 -0300

    initial pwa functionality :rocket:

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { PlayingCardsComponent } from './views/game/components/playing-cards/pla
 import { TimerComponent } from './views/game/components/timer/timer.component';
 import { ModeSelectorComponent } from './views/game/components/mode-selector/mode-selector.component';
 import { ModeMenuComponent } from './views/game/components/mode-menu/mode-menu.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,6 +32,12 @@ import { ModeMenuComponent } from './views/game/components/mode-menu/mode-menu.c
     HttpClientModule,
     SharedModule,
     ToastrModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
 
   providers: [
